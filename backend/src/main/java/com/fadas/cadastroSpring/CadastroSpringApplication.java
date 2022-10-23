8b47652c7ce70e140d3b9651643daf7c32852750
unknown <zuraffa2015@gmail.com>
2022-10-23 15:15:15 -0300

    backend project created

---
+++ backend/src/main/java/com/fadas/cadastroSpring/CadastroSpringApplication.java
@@ -0,0 +1,13 @@
+package com.fadas.cadastroSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroSpringApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/fadas/cadastroSpring/CadastroSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fadas.cadastroSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
